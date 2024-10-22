5a71c22f94b448bfde90a15149c9f0ea29025f04
Elena <rlenka2010gmail.com>
2024-10-22 14:35:14 +0200

    navbar style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,13 @@ import React from "react"
 import { NavLink } from "react-router-dom"
 import { navLinks } from "../../navlinks"
 //import './Navbar.module.css'
-import './Navbar.module.scss'
+import s from './Navbar.module.scss'
 
 export const Navbar =()=>{
 
  
     return(
-     <nav >
+     <nav className={s.navStyle}>
      {navLinks.map((item, index)=>{
         return(<>
             <NavLink  to={item.link} key={index}>{item.title}</NavLink>
